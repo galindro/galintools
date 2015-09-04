a71cd9e7a2a254dfcdb8cde8ceae7ac48ddc8fa0
Bruno Galindro da Costa <bruno.galindro@gmail.com>
2015-09-04 13:20:37 -0300

    Put -y in apt-get install

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ if ! dpkg -l |grep -q zabbix-sender; then
 	wget ${ZABBIX_PACKAGE_URL}/${ZABBIX_PACKAGE_NAME} -O /tmp/${ZABBIX_PACKAGE_NAME}
 	dpkg -i /tmp/${ZABBIX_PACKAGE_NAME}
 	apt-get update
-	apt-get install zabbix-sender
+	apt-get install -y zabbix-sender
 fi
 
-apt-get install python-pip python-dev 
+apt-get install -y python-pip python-dev 
 pip install mysql-connector-python --allow-external mysql-connector-python
 
 ./build.sh
